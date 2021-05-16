c6e2fa011fe6b8695d866c7dbd0f31eb27a2b63a
Evan <1350747952@qq.com>
2021-05-16 20:47:29 +0800

    代码清理

--- algorithms/name_to_method.m
+++ algorithms/name_to_method.m
@@ -9,20 +9,9 @@ function [method, distr, iterative, ZF_est] = name_to_method(name)
             ZF_est = true;
         case 'MMSE'
             method = @MMSE;
-        case 'QMMSE'
-            method = @QMMSE;
-            distr = true;
-        case 'BDAC-MMSE'
-            method = @QQMMSE;
-            distr = true;
         case 'DMMSE'
             method = @DMSE;
             distr = true;
-        case 'BCD-MMSE'
-            method = @BCDMMSE;
-            distr = true;
-            iterative = true;
-
         otherwise
             error('method type not defined.');
     end
--- algorithms/name_to_method_downlink.m
+++ algorithms/name_to_method_downlink.m
@@ -9,26 +9,6 @@ function [method, distr, iterative] = name_to_method_downlink(name)
             method = @ZFP;
         case 'DZF'
             method = @ZFP;
-        case 'cWMMSE'
-            method = @WMMSE;
-        case 'WMMSE'
-            method = @WMMSE;
-
-        case 'dEZF'
-            method = @EZF;
-            distr = true;
-        case 'dZF'
-            method = @ZFP;
-            distr = true;
-        case 'dWMMSE'
-            method = @WMMSE;
-            distr = true;
-            iterative = true;
-        case 'R-WMMSE'
-            method = @R_WMMSE;
-            distr = true;
-            iterative = true;
-
         otherwise
             error('method type not defined.');
     end
